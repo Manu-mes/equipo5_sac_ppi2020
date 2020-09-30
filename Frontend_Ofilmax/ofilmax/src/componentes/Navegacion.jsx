dee573d20721d11d47246adb8ad9864ee3c2ecbd
ximelopez06 <ximel862@gmail.com>
2020-09-30 13:39:59 -0500

    Modificacion en navegacion

--- Frontend_Ofilmax/ofilmax/src/componentes/Navegacion.jsx
+++ Frontend_Ofilmax/ofilmax/src/componentes/Navegacion.jsx
@@ -23,6 +23,9 @@ class Navegacion extends React.Component {
               </button>
               <div className="collapse navbar-collapse" id="navbarText">
                 <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                    <a className="nav-link active" href="#">Bienvenida</a>
+                  </li>
                   <li className="nav-item">
                     <a className="nav-link active" href="#">Inicio</a>
                   </li>
