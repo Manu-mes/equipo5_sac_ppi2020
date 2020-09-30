5474848c708d66eb56d5c7aa68f81bc88438979f
alexanderdj2307 <alexanderdj1234@gmail.com>
2020-09-30 17:00:35 -0500

    Panel

--- Frontend_Ofilmax/ofilmax/src/pagina/Panel_ev.jsx
+++ Frontend_Ofilmax/ofilmax/src/pagina/Panel_ev.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import '../Componentes/Navegacion'
+
 
 
 class Panel_ev extends React.Component {
     render() {
         return (
             <div>
-               
-
+                <Navegacion />
             </div>
         );
     }
