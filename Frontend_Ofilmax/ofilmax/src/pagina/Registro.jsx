47a1a6f2e6582292d73f7f3850048eaa3c0f73fc
alexanderdj2307 <alexanderdj1234@gmail.com>
2020-09-30 16:47:48 -0500

    C

--- Frontend_Ofilmax/ofilmax/src/pagina/Registro.jsx
+++ Frontend_Ofilmax/ofilmax/src/pagina/Registro.jsx
@@ -10,7 +10,7 @@ class Registro extends React.Component {
               <div className="col"></div>
               <div className="col-6 container vh-100  mt-4 text-center">
               <img src="https://lh5.googleusercontent.com/TU9XRhF2VGXOk6P7P0Yf4rNM1qTFZPhhoK0y10gy3OBI1zCWB1ocRsS6vFViON3PZPiaBpIvYw=w1280" className="rounded" alt="" width= "170" height="160"/>
-                <h5 className="text-center mt-4">Regis</h5>
+                <h5 className="text-center mt-4">Registro</h5>
                 <br />
                 <form >
                   <div className="form-group">
@@ -47,7 +47,7 @@ class Registro extends React.Component {
                       <label for="exampleFormControlSelect1">Elige tu Rol</label>
                       <select className="form-control" id="exampleFormControlSelect1">
                           <option>Estudiante</option>
-                          <option>Docente</option>
+                          <option>Do</option>
                           </select>
                 </div>
                   <Link to="/Bienvenida" button className=" btn btn-orange btn btn-info btn-block" type="submit">
