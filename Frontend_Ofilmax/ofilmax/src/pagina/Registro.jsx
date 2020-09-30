06d962ad8cafe1f653db6a7fb9a94945bc067ace
alexanderdj2307 <alexanderdj1234@gmail.com>
2020-09-30 16:49:42 -0500

    Cambio en el registro

--- Frontend_Ofilmax/ofilmax/src/pagina/Registro.jsx
+++ Frontend_Ofilmax/ofilmax/src/pagina/Registro.jsx
@@ -33,16 +33,7 @@ class Registro extends React.Component {
                       required
                     />
                   </div>
-                  <div className="form-group">
-                    <label>Confirma tu Contraseña</label>
-                    <input
-                      type="password"
-                      name="contrasena"
-                      id="inputPassword"
-                      className="form-control"
-                      required
-                    />
-                  </div>
+            
                   <div className="form-group">
                       <label for="exampleFormControlSelect1">Elige tu Rol</label>
                       <select className="form-control" id="exampleFormControlSelect1">
@@ -51,7 +42,7 @@ class Registro extends React.Component {
                           </select>
                 </div>
                   <Link to="/Bienvenida" button className=" btn btn-orange btn btn-info btn-block" type="submit">
-                    <strong>Registro</strong>
+                    <strong>Registrarme</strong>
                   </Link>
                 </form>
                 <br />
