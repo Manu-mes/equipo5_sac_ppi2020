fab39cf5ef0c0e6b3b48be8f8058832571fdf370
ximelopez06 <ximel862@gmail.com>
2020-10-01 16:48:48 -0500

    Route

---
+++ Frontend_Ofilmax/ofilmax/src/componentes/Aplicacion.jsx
@@ -0,0 +1,24 @@
+import Registro from "../pagina/Registro";
+import Bienvenida from "../pagina/Bienvenida";
+import Panel_ev from "../pagina/Panel_ev";
+
+
+
+function Aplicacion() {
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route exact path= '/' component= {PaginaInicial} />
+                <Route exact path= '/Inicio' component= {Inicio} />
+                <Route exact path= '/Registro' component= {Registro} />
+                <Route exact path= '/Bienvenida' component= {Bienvenida} />
+                <Route exact path= '/Panel_ev' component= {Panel_ev} />
+                
+            </Switch>
+        </BrowserRouter>
+
+    )
+    
+}
+
+export default Aplicacion;
