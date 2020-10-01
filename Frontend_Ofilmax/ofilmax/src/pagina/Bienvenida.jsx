10858a89fe0e7e95304a64ccf3d590beb5cd9fdd
ximelopez06 <ximel862@gmail.com>
2020-10-01 17:17:36 -0500

    Cmabio

--- Frontend_Ofilmax/ofilmax/src/pagina/Bienvenida.jsx
+++ Frontend_Ofilmax/ofilmax/src/pagina/Bienvenida.jsx
@@ -13,7 +13,7 @@ class Bienvenida extends React.Component {
                 <div className="card text-center " >
                     <img src="https://lh5.googleusercontent.com/-qiQnVrnubNKsI5NJbUzcMwHMdj1cRVdHYulLaVH3iA3EjGrqPQ_yZk1Lb1dp-uzIxQg2paFGg=w1280" className="rounded col-md-2 offset-md-5" alt="" width="250" height="250"/>
                     <div className="card-body">
-                        <h5 className="card-title">¡Bienvenidos a Ofilmax!</h5>
+                        <h5 className="card-title">¡Bienvenidos a Ofilmax</h5>
                         <p className="card-text">Aquí evaluaremos tus conocimientos sobre las herramientas Ofimaticas,Word, PowerPoint y Excel.</p>
                         <p className="card-text">En la parte superior encontraras la barra de navegación, con los servicios disponibles <br></br> según el Rol que elegiste en tu registro.</p>
                         <Link to="/" button className=" btn btn-orange btn btn-info btn-block" type="submit">
