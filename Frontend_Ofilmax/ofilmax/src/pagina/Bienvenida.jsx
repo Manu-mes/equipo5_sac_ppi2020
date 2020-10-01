17ce10bb27eaae5c43a638114f1e112ccdd49481
ximelopez06 <ximel862@gmail.com>
2020-10-01 16:59:36 -0500

    Bienvenida imagen

--- Frontend_Ofilmax/ofilmax/src/pagina/Bienvenida.jsx
+++ Frontend_Ofilmax/ofilmax/src/pagina/Bienvenida.jsx
@@ -11,7 +11,7 @@ class Bienvenida extends React.Component {
                 <br></br>
                 <div className="container">
                 <div className="card text-center " >
-                    <img src="" alt="" width="250" height="250"/>
+                    <img src="https://lh5.googleusercontent.com/-qiQnVrnubNKsI5NJbUzcMwHMdj1cRVdHYulLaVH3iA3EjGrqPQ_yZk1Lb1dp-uzIxQg2paFGg=w1280" alt="" width="250" height="250"/>
                     <div className="card-body">
                         <h5 className="card-title">¡Bienvenidos a Ofilmax!</h5>
                         <p className="card-text">Aquí evaluaremos tus conocimientos sobre las herramientas Ofimaticas,Word, PowerPoint y Excel.</p>
