ab51a472730b80d602424325b94718d93f80b927
ValentinaCaicedo <valentinacr2567@gmail.com>
2020-10-13 16:05:28 -0500

    Cambios

--- Frontend_Ofilmax/ofilmax/src/pagina/Resultados.jsx
+++ Frontend_Ofilmax/ofilmax/src/pagina/Resultados.jsx
@@ -15,7 +15,7 @@ class Resultados extends React.Component {
                             <div className="card-body">
                         <div className="card-body">
                             <h5 className="card-title">Herramienta Ofimatica: Word</h5>
-                            <p className="card-text">Evaluación de Word. 10/9/2020  5:30 pm<br></br>Puntaje: 5/7  ALTO</p>
+                            <p className="card-text">Evaluación de Word. 10/9/2020  5:30 pm<br></br>Puntaje: 5/7  MEDIO</p>
                             <a href="/Word" class="btn btn-outline-info">Ver mas...</a>
                         </div>
                         <div className="card-footer text-muted">
