94832db40e4a433f7dee7e56350f9a832a71345c
ximelopez06 <ximel862@gmail.com>
2020-10-13 20:02:30 -0500

    div

--- Frontend_Ofilmax/ofilmax/src/pagina/PowerPoint.jsx
+++ Frontend_Ofilmax/ofilmax/src/pagina/PowerPoint.jsx
@@ -107,7 +107,7 @@ class PowerPoint extends React.Component {
                         <div className="container">
                             <div className="col-md-auto ">
                                 <div className=" card-deck row justify-content-md-center ">
-                                    <div className="card w-50 text-center card border-primary mb-3">
+                                    <div className="card w-50 text-center card border-danger mb-3">
                                         <div className="card-body ">
                                             <h5 className="card-title text-primary">Pregunta N°4</h5>
                                             <p className="card-text">¿Cuál es la tecla que activa la presentacion?</p>
@@ -137,7 +137,7 @@ class PowerPoint extends React.Component {
                         <div className="container">
                             <div className="col-md-auto ">
                                 <div className=" card-deck row justify-content-md-center ">
-                                    <div className="card w-50 text-center card border-primary mb-3">
+                                    <div className="card w-50 text-center card border-danger mb-3">
                                         <div className="card-body ">
                                             <h5 className="card-title text-primary">Pregunta N°5</h5>
                                             <p className="card-text">¿En Power Point, podemos personalizar nuestra presentación?</p>
