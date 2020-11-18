4118ae20065bfe39927415c675d96cbc7ddef4ce
ValentinaCaiced <valentinacr2567@gmail.com>
2020-11-18 22:17:56 +0000

    Cambio de base de datos con la información

--- Backend_Ofilmax/db/mysql_pool.js
+++ Backend_Ofilmax/db/mysql_pool.js
@@ -2,10 +2,10 @@ mysql = require('mysql');
 const util = require('util')
 var pool  = mysql.createPool({
   connectionLimit : 10,
-  host     : 'bvorwk7lqvpsyxsgpn7a-mysql.services.clever-cloud.com',
-  user     : 'uujkvcjxqiigthvk',
-  password : 'mJUFZjGbQl88C9shhKHq',
-  database : 'bvorwk7lqvpsyxsgpn7a'
+  host     : 'bpt7ewe3rq1b02qisczd-mysql.services.clever-cloud.com',
+  user     : 'u8gwdf7vynq4ll5a',
+  password : 'hy3vHN8ko3UbkbAawVRP',
+  database : 'bpt7ewe3rq1b02qisczd'
 });
 
 pool.on('release', function (connection) {
