7edee26d230488d7ce856747c525ab13fb9a211a
Ximelopez06 <ximel862@gmail.com>
2020-11-24 14:18:09 +0000

    backend estudiantes

--- Backend_Ofilmax/routes/estudiantes.js
+++ Backend_Ofilmax/routes/estudiantes.js
@@ -14,8 +14,29 @@ router.get('/estudiantes/:Correo', (req, res) => {
    })
 })
 
-router.post('/estudiantes', (req, res) => {
-  
+router.post('/estudiantes/Inicio', (req, res)=>{
+  try{
+      const {
+        Contrasena,
+        Correo
+      } = req.body
+      connection.query('SELECT * FROM estudiantes WHERE Correo = ? AND Contrasena = SHA1(?)', [Correo, Contrasena], (error,result,fields) => {
+          if(error){
+            res.status(500).json({mensaje : "Se presento un error en la consulta"})
+          }else{
+            if(result.length > 0){
+               res.json({mensaje : 'El usuario supero la autenticación', Inicio : true, estudiantes : result[0]})
+            }else{
+               res.json({mensaje : 'Este usuario no existe', login : false})
+            }
+            console.log(result)
+           
+          }
+      })
+  }catch(error){
+    console.log(error)
+    res.status(503).json({ mensaje: "Error en el servidor.", error: true })
+  }
 })
 
 router.put('/estudiantes/:id', (req, res) => {})
