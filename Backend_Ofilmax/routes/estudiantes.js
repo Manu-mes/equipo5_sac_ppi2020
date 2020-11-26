8d3df09ed81e7f127d9902edd54120ee7251a143
ValentinaCaiced <valentinacr2567@gmail.com>
2020-11-26 23:48:19 +0000

    Servicios de estudiantes en el backend

--- Backend_Ofilmax/routes/estudiantes.js
+++ Backend_Ofilmax/routes/estudiantes.js
@@ -2,17 +2,103 @@ const {Router} = require('express')
 const router = Router()
 const {connection} = require('./../db/mysql_pool')
 
-router.get('/estudiantes', (req, res) => {
+
+router.get('/estudiante', (req, res) => {
   connection.query("SELECT * FROM estudiante", (error, result, fields) => {
-    res.json(result)
+    if(error){
+      console.log(error)
+      res.status(500).json({mensaje : "Error durante la consulta"})
+    }else{
+      res.json(result)
+    }
    })
 })
 
-router.get('/estudiantes/:Correo', (req, res) => {
-  connection.query("SELECT * FROM estudiante WHERE Correo = ? ",[req.params.Correo], (error, result, fields) => {
-    res.json(result)
+router.get('/estudiante/:correo', (req, res) => {
+  let correo = req.params.correo
+  connection.query("SELECT * FROM estudiante WHERE Correo = ?",[Correo] ,(error, result, fields) => {
+    if(result[0])
+      res.json(result[0])
+    else
+      res.json({})
    })
 })
+
+router.post('/estudiante', (req, res) => {
+  try{
+    let {
+      Correo,
+      Contrasena,
+      Numero_lista
+    } = req.body
+    const SQL = `INSERT INTO estudiante(Correo,Contrasena,Numero_lista) VALUES(?,?,?)`
+    const data = [Correo, Contrasena, Numero_lista]
+    connection.query(SQL, data,(error, result, fields) => {
+      if(error){
+      console.log(error)
+      res.status(500).json({mensaje : "Error durante la consulta"})
+    }else{
+      res.json({mensaje : "estudiante insertado correctamente."})
+    }
+    })
+  }catch(error){
+    console.log(error)
+      res.status(500).json("Error")
+  }
+})
+
+router.put('/estudiante/:correo', (req, res) => {
+  try{
+    let Correo = req.params.correo
+    let {
+      Contrasena,
+      Numero_lista
+    } = req.body
+    const SQL = `UPDATE estudiante SET Contrasena = ?,Numero_lista = ? WHERE Correo = ?`
+    const data = [Contrasena, Numero_lista,Correo]
+    connection.query(SQL, data,(error, result, fields) => {
+      if(error){
+      console.log(error)
+      res.status(500).json({mensaje : "Error durante la consulta"})
+    }else{
+      res.json({mensaje : "estudiante actualizado correctamente."})
+    }
+    })
+  }catch(error){
+    console.log(error)
+      res.status(500).json("Error")
+  }
+})
+
+router.delete('/estudiante/:correo', (req, res) => {
+  try{
+    let   Correo = req.params.correo
+  
+    const SQL = `DELETE FROM estudiante WHERE Correo = ?`
+    const data = [Correo]
+    connection.query(SQL, data,(error, result, fields) => {
+      if(error){
+      console.log(error)
+      res.status(500).json({mensaje : "Error durante la consulta"})
+    }else{
+      console.log(result)
+      if(result.affectedRows > 0)
+        res.json({mensaje : "estudiante eliminado correctamente."})
+      else
+        res.json({mensaje : "estudiante no existe con este correo o ya fue eliminado."})
+    }
+    })
+  }catch(error){
+    console.log(error)
+      res.status(500).json("Error")
+  }
+})
+
+
+
+
+
+////
 
 router.post('/estudiantes/Inicio', (req, res)=>{
   try{
